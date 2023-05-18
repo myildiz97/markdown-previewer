a55385d116f2623224f9170b64e5ba827df8b060
myildiz97 <myildizwork@gmail.com>
2023-05-18 18:48:40 +0300

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -33,7 +33,7 @@ function App() {
 
   > blockquote
 
-  ![Nature Image](image.jpg)
+  ![Nature Image](https://images.unsplash.com/photo-1684336177311-b34743d99c5c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=702&q=80)
 
   ***bold text**
 
